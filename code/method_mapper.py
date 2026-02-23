1ae21285dba2f88775e5a6f7db6ec118914739f1
eilwang <eileen.wang@czbiohub.org>
2026-02-22 21:42:58 -0800

    update method mapper
    Former-commit-id: e4a5ac0f36f9fe716d77602eb4c7ac0c85114704

--- code/method_mapper.py
+++ code/method_mapper.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
-def map_lcms_methods(collection, sequence, existing='fillna'):
+def map_lcms_methods(collection, sequences, existing='fillna'):
     """In place function to map LCMS methods to DiannCollection from sequence csv file"""
-    df = pd.read_csv(sequence)
-    df['lc meth'] = df['Instrument Method'].str.extract(r'.+\\(.+)$')
-    df['sample'] = df['Result Path'].str.extract(r'.+\\(.+)\.d+')
-    df['ms meth'] = df['MS Method'].str.extract(r'.+\\(.+?)(?:\.proteoscape)*\.m')
+    df_list = []
+    for sequence in sequences:
+        df = pd.read_csv(sequence)
+        df['lc meth'] = df['Instrument Method'].str.extract(r'.+\\(.+)$')
+        df['sample'] = df['Result Path'].str.extract(r'.+\\(.+)\.d+')
+        df['ms meth'] = df['MS Method'].str.extract(r'.+\\(.+?)(?:\.proteoscape)*\.m')
+        df_list.append(df)
+    df = pd.concat(df_list, ignore_index=True)
 
     #TODO: add function for filling in select missing valuees, ie if one sequences doesn't cover everything
     
